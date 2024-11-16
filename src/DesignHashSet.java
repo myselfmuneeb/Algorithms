bd1168de293cffab3ddd0c2a7050fdd8c5061f8e
Muneeb <myselfmuneeb>
2024-11-16 11:09:53 +0500

    Changes

--- src/DesignHashSet.java
+++ src/DesignHashSet.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 public class DesignHashSet {
     public static void main(String[] args) {
 
     }
 }
+class MyHashSet {
+
+    private static final int LIMIT = (int)Math.pow(10,6) + 1;
+    private final int[] set ;
+    int iterator =  0;
+
+    public MyHashSet() {
+        set = new int[LIMIT];
+        Arrays.fill(set, -1);
+    }
+
+    public void add(int key) {
+        set[key] = key;
+    }
+
+    public void remove(int key) {
+        set[key] = -1;
+    }
+
+    public boolean contains(int key) {
+        return set[key] != -1;
+    }
+}
+
 
