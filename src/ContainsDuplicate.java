358c01b818e14ac3975c8b4fedc3551cf7e832b9
Muneeb <myselfmuneeb>
2024-11-22 20:31:44 +0500

    Contain Duplicate Algorithm

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -6,20 +7,9 @@ public class ContainsDuplicate {
 
     public static void main(String[] args) {
 
-        int[] num = new int[(int)Math.pow(10, 5) + 1];
-        for (int i = 0; i < num.length; i++) {
-            num[i] = i;
-        }
-        if(containsDuplicate(num)){
-            System.out.println("Duplicate found");
-        }else {
-            System.out.println("Duplicate not found");
-        }
-
     }
-    public static boolean containsDuplicate(int[] nums) {
-        if(nums == null || nums.length > Math.pow(10, 5) +1 ) return false;
-        Map<Integer, Integer> map = new Hashtable<>();
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
