ac19ed6750fa77e98cfbe473cd05b299b77fe2a7
Muneeb <muneebbutt444@gmail.com>
2024-11-29 20:40:21 +0500

    Squares of Sorted Array

---
+++ src/SquaresOfSortedArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-4, -1, 0, 3, 10})));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
